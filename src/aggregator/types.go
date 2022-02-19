baf810bb614b4ed3af20bd07209cfc9bc44ac962
Joni Kähärä <joni.kahara@async.fi>
2022-02-19 22:22:17 +0200

    add Aggregation type

--- src/aggregator/types.go
+++ src/aggregator/types.go
@@ -1,4 +1,17 @@
 package aggregator
 
-type Aggregation interface {
+import "time"
+
+type Aggregation struct {
+	Time     time.Time
+	Key      string
+	Min      float64
+	Q1       float64
+	Q2       float64
+	Q3       float64
+	Max      float64
+	IQR      float64
+	MAD      float64
+	StdDev   float64
+	Variance float64
 }
