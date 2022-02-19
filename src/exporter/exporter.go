cc5c8caa366f3df1c7ec10f92c2cf541271a2bf9
Joni Kähärä <joni.kahara@async.fi>
2022-02-19 22:21:50 +0200

    start implementing the exporter

--- src/exporter/exporter.go
+++ src/exporter/exporter.go
@@ -1,7 +1,19 @@
 package exporter
 
-import "github.com/kahara/go-gnssaggr/src/aggregator"
+import (
+	"github.com/kahara/go-gnssaggr/src/aggregator"
+	"github.com/rs/zerolog/log"
+)
 
 func Export(aggregations <-chan aggregator.Aggregation) {
+	var (
+		aggregation aggregator.Aggregation
+	)
 
+	for {
+		select {
+		case aggregation = <-aggregations:
+			log.Printf("%+v", aggregation)
+		}
+	}
 }
